84b6e003580740d67a9595dd340be1939fcebf5f
Thiago Vieira <thiagovieira7@outlook.com>
2021-11-17 21:28:48 -0300

    criação code index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+require("dotenv").config();
 
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const Conn = require("./model/conn/index");
+
+Conn();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Rota Lojas Esportivas ok" });
+});
+
+const acessoriosRouter = require("./routers/acessorios.routes");
+app.use("/acessorios", acessoriosRouter);
+
+const calcadosRouter = require("./routers/calcados.routes");
+app.use("/calcados", calcadosRouter);
+
+const equipamentosRouter = require("./routers/equipamentos.routes");
+app.use("/equipamentos", equipamentosRouter);
+
+const roupasRouter = require("./routers/roupas.routes");
+app.use("/roupas", roupasRouter);
+
+const suplementosRouter = require("./routers/suplementos.routes");
+app.use("/suplementos", suplementosRouter);
+
+// app.listen(process.env.PORT, () => {
+//   console.info(`App rodando em http://localhost:${process.env.PORT}`);
+// }); usado para rodar com banco de dados
+
+app.listen(port, () => {
+  console.info(`App rodando em http://localhost:${port}`);
+});
