c6d4e94d32331a252f1738fd7dcc00581d2ad20a
Thiago Vieira <thiagovieira7@outlook.com>
2021-11-18 20:27:18 -0300

    create env && backup envi

--- model/conn/index.js
+++ model/conn/index.js
@@ -1,1 +1,19 @@
+const mongoose = require("mongoose");
 
+async function Conn() {
+  await mongoose
+    .connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_BASE}`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
+    .then(() => {
+      console.log("MongoDB conectado");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+module.exports = Conn;
