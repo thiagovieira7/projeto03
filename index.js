66cafc620e43cba39bc1a3942af3edd9e9da9b59
AndreiaLima19 <89053834+AndreiaLima19@users.noreply.github.com>
2021-11-18 20:41:58 -0300

    Rotas Roupas e Calcados

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-//teste
+const express = require("express");  //chamando o express
+const app = express();  //definindo o app como express
+require('dotenv').config(); // config do .env para acessar as VARs de ambiente
+const cors = require("cors");  // importando o CORS para nosso APP
+app.use(express.json());  //definindo o JSON no projeto
+
+const Conn = require("./model/conn/index"); //importando a conexao
+
+Conn(); //executa a func de conexao
+
+app.use(cors()); // usando o CORS no APP
+app.options("*", cors()); // Configurando o CORS para liberar todo trafego.
+
+app.get('/', (req,res) => {
+    res.status(200).json({message:"API Loja Esportiva"});
+});
+
+const RoupasRouter = require("./routers/roupas.routers");
+app.use("/roupas",RoupasRouter);
+
+const CalcadosRouter = require("./routers/calcados.routers");
+app.use("/calcados",CalcadosRouter);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`);
+});
