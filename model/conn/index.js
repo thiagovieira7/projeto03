70078f2cd0efe34e5ba0f75e320edf17e6c7d7c8
Thiago Vieira <thiagovieira7@outlook.com>
2021-11-22 10:18:24 -0300

    index
    update

--- model/conn/index.js
+++ model/conn/index.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");  //importando o mongoose
-
-async function Conn(){
-    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_BASE}`,{ 
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(() => { 
-        console.log("MongoDB esta conectado");
-    }).catch((err) => { 
-        console.error(err);
-    });
-}
-
-module.exports = Conn; //exporta a conexao
-=======
 const mongoose = require("mongoose");
 
 async function Conn() {
@@ -33,4 +17,3 @@ async function Conn() {
     });
 }
 module.exports = Conn;
->>>>>>> thiagoDevelop
